6fefd4eb9a287a56150d11742cade46cd1ffa003
Soomin5468 <159857386+Soomin5468@users.noreply.github.com>
2024-10-17 15:32:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ import streamlit as st
 st.write("Hello")
 st.write("Hello\n")
 st.write("Hello\n")
+
+st.write("Hello\n")
+
+st.write("Hello\n")
+
+st.write("Hello\n")
+st.write("Hello\n")
+st.write("Hello\n")
+st.write("Hello\n")
+st.write("Hello\n")
+st.write("Hello\n")
